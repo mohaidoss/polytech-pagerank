221692a67106b708a9e9c0675f7fad1978589a63
mmoussai <mehdi.moussaif@polytech-lille.net>
2021-09-09 15:46:55 +0200

    values are graph vertex

--- pageRank1.py
+++ pageRank1.py
@@ -2,12 +2,19 @@ import pandas as pd
 import igraph as gr
 
 dataframe = pd.read_csv('wikispeedia_paths-and-graph/paths_finished.tsv',header = None, sep = '\t', comment = '#')
-print(dataframe[3][0])
-list = []
+pathList = []
+values = []
 path=dataframe[3].dropna(how='any',axis=0)
 for i in range(path.size):
-    list += [(path[i].split(';'))]
-print(list[1])
-g = gr.Graph()
-g.TupleList(edges=list, directed=True)
-gr.plot(g)
+    values += path[i].split(';')
+    pathList += [(path[i].split(';'))]
+
+x = 50
+print(len(values))
+
+values = list(set(values))
+print(values)
+#g = gr.Graph([('Banana','Apple'),('eggs','Banana')], directed = True)
+#g.TupleList(edges=pathList[x], directed=True)
+#g.TupleList([('Banana','Apple'),('eggs','Banana')], directed = True)
+gr.plot(g, vertex_label = values)
