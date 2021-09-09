87a353d0efa54cfc471aa8676deb1815d7345f16
mmoussai <mehdi.moussaif@polytech-lille.net>
2021-09-09 10:33:09 +0200

    removed rows with null

--- pageRank1.py
+++ pageRank1.py
@@ -4,7 +4,10 @@ import igraph as gr
 dataframe = pd.read_csv('wikispeedia_paths-and-graph/paths_finished.tsv',header = None, sep = '\t', comment = '#')
 print(dataframe[3][0])
 list = []
-for i in range(dataframe[3].size):
-    list += [(dataframe[3][i].split(';'))]
-    
-gr.Graph.TupleList(edges=list)
+path=dataframe[3].dropna(how='any',axis=0)
+for i in range(path.size):
+    list += [(path[i].split(';'))]
+print(list[1])
+g = gr.Graph()
+g.TupleList(edges=list, directed=True)
+gr.plot(g)
