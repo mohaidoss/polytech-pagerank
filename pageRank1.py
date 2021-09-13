ab5f999555e0d826fe57c73bd4e820db2db343a3
mohaidoss <moussaif5@gmail.com>
2021-09-13 03:09:48 +0200

    Page rank with power method ok

--- pageRank1.py
+++ pageRank1.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import igraph as gr
+import numpy as np
 Beta_parameter = 0.85
-
+epsilon = 0.001
 
 def fixDepart(l):
     try:
@@ -62,10 +63,13 @@ gr.summary(g)
 def weight_source(g,vertex):#8 14th_century
     l = []
     source = vertex.attributes()['name']
-    for e in vertex.incident():
+    for e in vertex.incident(mode='out'):
         l += [g.vs[e.target].attributes()['name']]
     data = {x:l.count(x)/len(l) for x in l}
     return source, data
+#print(len(g.vs[1212].incident(mode='out')))
+#print('us')
+#print(len(g.vs[3893].incident()))
 
 df_index =[]
 data =[]
@@ -81,4 +85,34 @@ mx_markov.fillna(0, inplace = True) #Remplacer les NaN
 #print(mx_markov) 
 
 # PageRank algorithm
-#print(g.pagerank())
+print(np.argmax(g.pagerank()))
+
+
+# Calcul vecteur propre
+# A est une matrice numpy, r_vector est notre vecteur d'essai, Beta est le damping factor
+def powermethod(A,r_vector,Beta_parameter):
+    N = A.shape[0]
+    P_q = A.dot(r_vector)
+    r_vector = Beta_parameter*P_q + (1-Beta_parameter)*(np.sum(r_vector))/N
+    return r_vector
+
+
+
+
+# Starting vector
+#b_k_1 = np.random.rand(mx_markov.to_numpy().shape[1])
+b_k_1 = np.full(mx_markov.to_numpy().shape[1],1)
+print(b_k_1)
+
+# Stochastic matrix (P.T)
+A= mx_markov.to_numpy()
+
+# Power method
+b_k = powermethod(A.T,b_k_1,Beta_parameter)
+while ((b_k_1 - b_k) > epsilon).any() :
+    b_k_1 = b_k
+    b_k = powermethod(A.T,b_k_1,Beta_parameter)
+
+
+print(mx_markov.index[np.argmax(b_k)])
+#1212 - 3893
