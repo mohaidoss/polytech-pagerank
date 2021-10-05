f8ba16078339fcf2bd7957dfa0970ae74752eee7
mohaidoss <moussaif5@gmail.com>
2021-10-06 01:53:51 +0200

    Page rank Personnalisé

--- pageRank1.py
+++ pageRank1.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import igraph as gr
 import numpy as np
+import time 
+
 Beta_parameter = 0.85
 epsilon = 0.001
 
@@ -58,18 +60,26 @@ graph_df['end'] = end
 g = gr.Graph.DataFrame(graph_df)
 #gr.plot(g, vertex_size = 10, edge_arrow_size = 0.4, edge_width = 0.5)
 gr.summary(g)
+# PageRank avec iGraph
+# print(g.pagerank())
 
+A = np.array(g.get_adjacency().data).T
+sums = A.sum(axis=0, keepdims = 0)
+sums[sums == 0] = 1
+
+A_norm = A/sums
+N = A_norm.shape[1]
+
+# Une deuxième méthode de calcul de la matrice de transition, en utilisant la chaine de markov
+"""
 #Fonction qui retourne un dictionnaire pour chaque source donnée avec chaine de markov (target[key], probability[value])
-def weight_source(g,vertex):#8 14th_century
+def weight_source(g,vertex):
     l = []
     source = vertex.attributes()['name']
     for e in vertex.incident(mode='out'):
         l += [g.vs[e.target].attributes()['name']]
     data = {x:l.count(x)/len(l) for x in l}
     return source, data
-#print(len(g.vs[1212].incident(mode='out')))
-#print('us')
-#print(len(g.vs[3893].incident()))
 
 df_index =[]
 data =[]
@@ -84,35 +94,56 @@ mx_markov = pd.DataFrame(data, index=df_index, columns=df_index)    # index = co
 mx_markov.fillna(0, inplace = True) #Remplacer les NaN
 #print(mx_markov) 
 
-# PageRank algorithm
-print(np.argmax(g.pagerank()))
-
+# Stochastic matrix (P.T)
+#A_norm= mx_markov.to_numpy().T
+"""
 
 # Calcul vecteur propre
-# A est une matrice numpy, r_vector est notre vecteur d'essai, Beta est le damping factor
-def powermethod(A,r_vector,Beta_parameter):
-    N = A.shape[0]
+# A est une matrice numpy, r_vector est notre vecteur d'essai, Beta est le damping factor, k est notre nombre de noeud (k!=N si PageRank persionnalisé)
+def powermethod(A,r_vector,Beta_parameter,k):
     P_q = A.dot(r_vector)
-    r_vector = Beta_parameter*P_q + (1-Beta_parameter)*(np.sum(r_vector))/N
+    r_vector = Beta_parameter*P_q + (1-Beta_parameter)*(np.sum(r_vector))/k
     return r_vector
 
 
 
 
-# Starting vector
-#b_k_1 = np.random.rand(mx_markov.to_numpy().shape[1])
-b_k_1 = np.full(mx_markov.to_numpy().shape[1],1)
-print(b_k_1)
+# Initialisation du vecteur
+b_k_1 = np.full(N,1)
 
-# Stochastic matrix (P.T)
-A= mx_markov.to_numpy()
+# Méthode de la puissance - execution
+start_time = time.time()
+b_k = powermethod(A_norm,b_k_1,Beta_parameter,N)
 
-# Power method
-b_k = powermethod(A.T,b_k_1,Beta_parameter)
+iter = 1
 while ((b_k_1 - b_k) > epsilon).any() :
     b_k_1 = b_k
-    b_k = powermethod(A.T,b_k_1,Beta_parameter)
+    b_k = powermethod(A_norm,b_k_1,Beta_parameter,N)
+    iter += 1
+
+# Temps d'execution de l'algorithme et nombre d'itération
+print(time.time() - start_time, iter)
+
+# Liste finale du PageRank, avec le nom des pages en index
+ranking_df = pd.DataFrame(b_k,index=g.vs()["name"], columns=['rank'])
+
+print(ranking_df.sort_values(by=['rank'], ascending=False).head(n=20))
 
 
-print(mx_markov.index[np.argmax(b_k)])
-#1212 - 3893
+
+# PageRank Personnalisé
+# Mehdi Moussaif : M.M ==> un seul noeud : ligne 26
+print(ranking_df.index[13])
+# Initialisation du vecteur
+b_k_1 = np.full(N,0)
+b_k_1[13] = 1
+
+# Méthode de la puissance avec 1 noeud - execution
+b_k = powermethod(A_norm,b_k_1,Beta_parameter,1)
+
+while ((b_k_1 - b_k) > epsilon).any() :
+    b_k_1 = b_k
+    b_k = powermethod(A_norm,b_k_1,Beta_parameter,1)
+    iter += 1
+ranking_df['rank'] = b_k
+print(ranking_df.sort_values(by=['rank'], ascending=False).head(n=20))
